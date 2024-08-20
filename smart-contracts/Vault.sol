ca3f254b22d2e554f3ad9328cbbf718c7021b051
linglanttian <265847281+linglanttian@users.noreply.github.com>
2024-08-20 13:38:10 +0700

    feat: wire up message broker publisher in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -41,3 +41,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 5267
 // Optimized logic batch 5468
 // Optimized logic batch 5459
+// Optimized logic batch 2982
